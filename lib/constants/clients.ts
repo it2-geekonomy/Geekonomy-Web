742a35803cd6e941b6bc92c3b60928d72f822cc8
Srivalli-Yaarlagadda <srivalli@geekonomy.in>
2026-02-25 17:30:43 +0530

    updated industryservice images

--- lib/constants/clients.ts
+++ lib/constants/clients.ts
@@ -1,29 +1,25 @@
 export const CLIENTS = [
   {
     name: "DivyaSree",
-    logo: "/IndustryServicesImages/divyasreelogo.png",
-    image: "/case-studies/Divyasree.webp",
+    image: "/IndustryServicesImages/divyashree.webp",
     width: 180,
     height: 100,
   },
   {
     name: "Musashi",
-    logo: "/IndustryServicesImages/mushashilogo.png",
-    image: "/case-studies/Musashi.webp",
+    image: "/IndustryServicesImages/musashi .webp",
     width: 130,
     height: 50,
   },
   {
     name: "Hindustan Power",
-    logo: "/IndustryServicesImages/hindusthanlogo.png",
-    image: "/case-studies/Hindusthanpower.webp",
+    image: "/IndustryServicesImages/hindustanpower.webp",
     width: 240,
     height: 100,
   },
   {
     name: "VST Group",
-    logo: "/IndustryServicesImages/VSTlogo.png",
-    image: "/case-studies/VST Group1.webp",
+    image: "/IndustryServicesImages/VST.webp",
     width: 200,
     height: 140,
   },
