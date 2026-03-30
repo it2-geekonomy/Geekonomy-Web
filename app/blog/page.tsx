2d41b396cb501743ed67b514989579d08038f76d
Kishan <kishan@geekonomy.in>
2026-03-30 17:45:00 +0530

    loading animation added

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,6 @@
 import { Suspense } from "react";
 import BlogsPageClient from "./BlogsPageClient";
-import { Typography } from "@/components/ui/Typography";
-
-function BlogsPageLoading() {
-  return (
-    <main className="bg-black min-h-screen py-[clamp(2.5rem,2.5rem+2vw,8rem)] flex items-center justify-center">
-      <Typography as="p" variant="body-xl" className="text-white">
-        Loading...
-      </Typography>
-    </main>
-  );
-}
+import BlogsPageLoading from "./BlogsPageLoading";
 
 export default function BlogsPage() {
   return (
