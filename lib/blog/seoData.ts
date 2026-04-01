6421e04051591417f54f3d1f118f093c7bcac698
Kishan <kishan@geekonomy.in>
2026-04-01 17:50:30 +0530

    .

--- lib/blog/seoData.ts
+++ lib/blog/seoData.ts
@@ -151,10 +151,7 @@ import { howtoHireBrandAmbassadorsforYourBusinessSEO } from "./data/howtoHireBra
 import {HowGoogleDisplayAdsGrowMarketingResultsforAdvertisersSEO} from "./data/howGoogleDisplayAdsGrowMarketingResultsforAdvertisers";
 import { howtoUseOnlineReviewsforRealEstateAppraiserMarketingSEO } from "./data/howtoUseOnlineReviewsforRealEstateAppraiserMarketing";
 import { howtoUseSocialListeningforInfluencerMarketingSEO } from "./data/howtoUseSocialListeningforInfluencerMarketing";
-<<<<<<< HEAD
-=======
 import { heavyvehicletrainingbusinessmarketingstrategiesSEO } from "./data/heavyvehicletrainingbusinessmarketingstrategies";
->>>>>>> 7b97e76a81dd498d5147da37609367b442742b3c
 /** All blog SEO data aggregated by slug key (blog/{slug}) */
 export const blogSEOData = {
   "blog/ben-stace-semantic-seo-writing-tool": benStaceSemanticSEOWritingToolBoostYourRankingSEO,
