22f70abcec6a54c86618c036b0072bf66998f001
vikas kumhar <vikaskumhar811@gmail.com>
2026-09-04 14:41:29 +0530

    removed footer from landing pages

---
+++ lib/constants/Hidefooterroutes.ts
@@ -0,0 +1,15 @@
+// Routes where the entire footer should be hidden
+export const HIDE_FOOTER_ROUTES = [
+  "/digital-marketing-for-pool-companies-san-diego",
+  "/carlsbad-seo-company",
+  "/charlotte-ppc-agency",
+  "/digital-marketing-for-logistics-companies",
+  "/digital-marketing-for-window-and-door-companies",
+  "/digital-marketing-for-paving-companies",
+  "/digital-marketing-agency-ocean-springs-ms",
+  "/seo-company-carmel-indiana",
+  "/digital-marketing-company-in-jaipur",
+  "/digital-marketing-company-in-ghaziabad",
+  "/digital-marketing-company-in-madurai",
+  "/digital-marketing-agency-guwahati"
+];
